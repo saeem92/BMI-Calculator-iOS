8eb694851aa0b832b2eaee86dd6b5398d3ecec48
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-01-20 20:55:04 +0530

    Initial Commit

---
+++ BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Saeem on 20/01/2022.
+//  Copyright © 2022 Saeem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
